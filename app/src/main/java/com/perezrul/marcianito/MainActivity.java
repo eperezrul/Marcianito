5e54a385e4ede3df983c3193b5b605b1b4959862
Edgard Perez Rul <eperezrul@gmail.com>
2016-09-07 13:05:20 -0500

    Mi Primer proyecto de Android
    Primer proyecto de Android del curso Fundamentos de Android

---
+++ app/src/main/java/com/perezrul/marcianito/MainActivity.java
@@ -0,0 +1,36 @@
+package com.perezrul.marcianito;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    Button button1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button1 = (Button) findViewById(R.id.MyButton);
+
+        // Capture button clicks
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+
+                // Start NewActivity.class
+                Intent myIntent;
+                myIntent = new Intent(MainActivity.this,
+                        Final_Activity.class);
+                startActivity(myIntent);
+            }
+        });
+    }
+
+
+}
